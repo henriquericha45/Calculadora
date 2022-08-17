49cfd863ba10aaca33676372d305264e04b2c977
henriquericha45 <henriquericha@hotmail.com>
2022-08-17 16:21:55 -0300

    atualizacao

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const display = document.querySelector('.display');
+
+function teste(){
+    console.log("deu boa");
+    var ttttt = 5;
+    document.getElementById("display").value = 5;
+    console.log("terminou");
+}
